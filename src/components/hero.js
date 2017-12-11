a347425266e0d26308cd768552b6c6b5bbf9c1ce
Andy Cochran <acochran@planning.nyc.gov>
2017-12-11 15:27:54 -0500

    add hero component

---
+++ src/components/hero.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+
+class Hero extends React.Component {
+ render() {
+   const title = this.props.title ? <h1 className="header-large">{ this.props.title }</h1> : null;
+
+   const tagline = this.props.tagline ? <p className="header-xlarge">{ this.props.tagline }</p> : null;
+
+   const button = this.props.buttonText && this.props.buttonURL ? <Link to={ this.props.buttonURL } className="button large">{ this.props.buttonText }</Link> : null;
+
+   return <div className="hero">
+     <div className="grid-container">
+       <div className="grid-x grid-padding-x">
+         <div className="cell">
+          {title}
+          {tagline}
+          {button}
+         </div>
+       </div>
+     </div>
+   </div>
+ }
+}
+
+export default Hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import Hero from '../components/hero'
 import {
     SkylineSVG,
     MonitorSVG,
@@ -14,50 +15,60 @@ class IndexPage extends React.Component {
 
   render() {
     return (
-      <div className="grid-container">
-        <div className="grid-x grid-padding-x">
-          <div className="cell">
+      <div>
 
-            <h2>Planning + Tech</h2>
+        <Hero
+          tagline="We work with New York City's Urban Planners to deliver impactful, modern technology tools."
+          buttonText="Get in touch"
+          buttonURL="/contact"
+          />
 
-            <div className="grid-x grid-margin-x">
-              <div className="cell large-auto">
-                <p className="lead">We believe better outcomes can be achieved using modern design and development practices along with open technology. We are civic technologists here to help support the Department of City Planning's mission.</p>
+        <div className="grid-container">
+          <div className="grid-x grid-padding-x">
+            <div className="cell">
+
+              <h2>Planning + Tech</h2>
+
+              <div className="grid-x grid-margin-x">
+                <div className="cell large-auto">
+                  <p className="lead">We believe better outcomes can be achieved using modern design and development practices along with open technology. We are civic technologists here to help support the Department of City Planning's mission.</p>
+                </div>
+                <div className="cell large-auto">
+
+                  <div className="media-object">
+                    <div className="media-object-section">
+                      <MonitorSVG />
+                    </div>
+                    <div className="media-object-section">
+                      <p className="header-medium">Bring civic data to life through interactive maps and visualizations</p>
+                    </div>
+                  </div>
+
+                  <div className="media-object">
+                    <div className="media-object-section">
+                      <SkylineSVG />
+                    </div>
+                    <div className="media-object-section">
+                      <p className="header-medium">Create tools to help New Yorkers better understand the built environment</p>
+                    </div>
+                  </div>
+
+                  <div className="media-object">
+                    <div className="media-object-section">
+                      <FolderSVG />
+                    </div>
+                    <div className="media-object-section">
+                      <p className="header-medium">Build simple web-based tools to streamline internal workflows</p>
+                    </div>
+                  </div>
+
+                </div>
               </div>
-              <div className="cell large-auto">
 
-                <div className="media-object">
-                  <div className="media-object-section">
-                    <MonitorSVG />
-                  </div>
-                  <div className="media-object-section">
-                    <p className="header-medium">Bring civic data to life through interactive maps and visualizations</p>
-                  </div>
-                </div>
-
-                <div className="media-object">
-                  <div className="media-object-section">
-                    <SkylineSVG />
-                  </div>
-                  <div className="media-object-section">
-                    <p className="header-medium">Create tools to help New Yorkers better understand the built environment</p>
-                  </div>
-                </div>
-
-                <div className="media-object">
-                  <div className="media-object-section">
-                    <FolderSVG />
-                  </div>
-                  <div className="media-object-section">
-                    <p className="header-medium">Build simple web-based tools to streamline internal workflows</p>
-                  </div>
-                </div>
-
-              </div>
             </div>
-
           </div>
         </div>
+
       </div>
     )
   }
