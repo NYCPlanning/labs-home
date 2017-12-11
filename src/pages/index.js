00599926d3e51f79e0552c1dadd15a21fb67cd14
allthesignals <wilburnforce@gmail.com>
2017-12-11 16:04:26 -0500

    Project pages, dynamic segments

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-class IndexPage extends React.Component {
-  componentWillMount() {
-    console.log('componentWillMount!!!')
-  }
-
+class IndexPage extends React.Component {  
   render() {
     return (
       <div>
