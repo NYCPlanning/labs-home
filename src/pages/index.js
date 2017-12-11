94835035adc2b959a8feb09f681fbc2f0a921bc6
allthesignals <wilburnforce@gmail.com>
2017-12-11 12:10:14 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+class IndexPage extends React.Component {
+  componentWillMount() {
+    console.log('componentWillMount!!!')
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Peanut Butter and Pickles</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    )
+  }
+}
+
+export default IndexPage
