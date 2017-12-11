b8fb3eb4b789168390d8bdea543ad7b25323f67d
allthesignals <wilburnforce@gmail.com>
2017-12-11 12:16:39 -0500

    add sass plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`
+  ],
 }
