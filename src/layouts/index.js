1f0656990abc59ca468c27c05bce4b799797ceba
Andy Cochran <acochran@planning.nyc.gov>
2017-12-13 10:05:29 -0500

    change sample site description, nix keywords meta

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,9 +13,7 @@ const TemplateWrapper = ({ children }) => (
     <Helmet
       title="NYC Planning Labs"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
+        { name: 'description', content: 'NYC Planning Labs builds open, impactful, modern technology tools for urban planners' 
     >
       <link href="https://fonts.googleapis.com/css?family=Montserrat:900" rel="stylesheet" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
