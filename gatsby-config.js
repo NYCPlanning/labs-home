ef993b3de0b204a4bd6835307cb319a337ef8aaa
trbmcginnis <taylormcgin@gmail.com>
2018-11-29 14:13:58 -0500

    add google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,12 @@ module.exports = {
     title: `NYC Planning Labs`,
   },
   plugins: [
+    {
+  resolve: `gatsby-plugin-google-analytics`,
+  options: {
+    trackingId: "UA-84250233-4",
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`
   ],
