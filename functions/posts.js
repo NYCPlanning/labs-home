d1c0f5b11715fbdf503ee37653e79fa90b5dd31c
allthesignals <wilburnforce@gmail.com>
2021-05-04 10:24:09 -0400

    Migrate private APIs to serverless funcs

---
+++ functions/posts.js
@@ -0,0 +1,35 @@
+const Feed = require("rss-to-json");
+const cheerio = require("cheerio");
+
+exports.handler = async event => {
+  const { tag, limit = 4 } = event.queryStringParameters;
+  let feedUrl;
+  if (tag) {
+    feedUrl = `https://medium.com/feed/nyc-planning-digital/tagged/${tag}?truncated=true`;
+  } else {
+    feedUrl = "https://medium.com/feed/nyc-planning-digital?truncated=true";
+  }
+
+  const rss = await Feed.load(feedUrl);
+
+  rss.items.map(item => {
+    const $ = cheerio.load(item.description);
+    const parsedDescription = $(".medium-feed-snippet").text();
+    const parsedImage = $(".medium-feed-image img").attr("src");
+
+    const newItem = item;
+    newItem.description = parsedDescription;
+    newItem.image = parsedImage;
+    return newItem;
+  });
+
+  rss.items = rss.items.slice(0, limit);
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(rss)
+  };
+};
--- src/components/blog-posts.js
+++ src/components/blog-posts.js
@@ -1,61 +1,62 @@
-import React from 'react';
-import moment from 'moment';
-import FontAwesome from 'react-fontawesome';
-import fetch from 'node-fetch';
+import React from "react";
+import moment from "moment";
+import FontAwesome from "react-fontawesome";
+import fetch from "node-fetch";
 
 class BlogPosts extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = { posts: null };
   }
 
-  componentDidMount () {
-    fetch('https://labs-home-api.herokuapp.com/posts?tag=nyc-planning-labs')
+  componentDidMount() {
+    fetch("/.netlify/functions/posts?tag=nyc-planning-labs")
       .then(response => response.json())
-      .then((json) => {
+      .then(json => {
         const posts = json.items;
         this.setState({ posts });
       });
   }
 
-  render () {
-    const renderPosts = posts => posts.map((post) => {
-      let postImage = null;
-      if (post.image) {
-        postImage = (
-          <p>
-            <a href={post.url}>
-              <img src={post.image} alt="blog post" />
-            </a>
-          </p>
-        );
-      }
+  render() {
+    const renderPosts = posts =>
+      posts.map(post => {
+        let postImage = null;
+        if (post.image) {
+          postImage = (
+            <p>
+              <a href={post.url}>
+                <img src={post.image} alt="blog post" />
+              </a>
+            </p>
+          );
+        }
 
-      return (
-        <div key={post.created} className="cell large-auto">
-          <span className="post-date">{moment(post.created).format('LL')}</span>
-          <h1 className="header-medium">
-            <a href={post.url}>{post.title}</a>
-          </h1>
-          {postImage}
-          <p className="post-excerpt">{post.description}</p>
-          <a
-            className="button small "
-            href={post.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read more
-            {' '}
-            <FontAwesome name="external-link" />
-          </a>
-        </div>
-      );
-    });
+        return (
+          <div key={post.created} className="cell large-auto">
+            <span className="post-date">
+              {moment(post.created).format("LL")}
+            </span>
+            <h1 className="header-medium">
+              <a href={post.url}>{post.title}</a>
+            </h1>
+            {postImage}
+            <p className="post-excerpt">{post.description}</p>
+            <a
+              className="button small "
+              href={post.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Read more <FontAwesome name="external-link" />
+            </a>
+          </div>
+        );
+      });
 
     const { posts } = this.state;
 
-    const renderedPosts = posts ? renderPosts(posts) : 'No Posts';
+    const renderedPosts = posts ? renderPosts(posts) : "No Posts";
 
     return <div className="grid-x grid-margin-x">{renderedPosts}</div>;
   }
